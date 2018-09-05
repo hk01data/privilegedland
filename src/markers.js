5b0d0b9772ce669e2f67c8283c945ccc82ff5916
HK01 Data <bigdata@hk01.com>
2018-09-05 16:55:11 +0800

    Add files via upload

--- src/markers.js
+++ src/markers.js
@@ -1,464 +1,275 @@
 var markers = {
     "type": "FeatureCollection",
     "features": [
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    114.1638609,
-                    22.2433111,
-                    0
-                ]
-            },
-            "properties": {
-                "name": "香港仔遊艇會",
-                "id": 1,
-                "Latitude": 22.2433111,
-                "Longitude": 114.1638609
-            }
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            114.1637886,
+            22.2433234,
+            0
+          ]
         },
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    114.1921453,
-                    22.2798835,
-                    0
-                ]
-            },
-            "properties": {
-                "name": "中華游樂會",
-                "id": 2,
-                "Latitude": 22.2798835,
-                "Longitude": 114.1921453
-            }
-        },
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    114.2986563,
-                    22.2641186,
-                    0
-                ]
-            },
-            "properties": {
-                "name": "清水灣鄉村俱樂部",
-                "id": 3,
-                "Latitude": 22.2641186,
-                "Longitude": 114.2986563
-            }
-        },
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    114.176029,
-                    22.307056,
-                    0
-                ]
-            },
-            "properties": {
-                "name": "西洋波會",
-                "id": 4,
-                "Latitude": 22.307056,
-                "Longitude": 114.176029
-            }
-        },
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    114.1820791,
-                    22.27677,
-                    0
-                ]
-            },
-            "properties": {
-                "name": "紀利華木球會",
-                "id": 5,
-                "Latitude": 22.27677,
-                "Longitude": 114.1820791
-            }
-        },
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    114.1768018,
-                    22.308,
-                    0
-                ]
-            },
-            "properties": {
-                "name": "菲律賓會",
-                "id": 6,
-                "Latitude": 22.308,
-                "Longitude": 114.1768018
-            }
-        },
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    114.259561,
-                    22.36479,
-                    0
-                ]
-            },
-            "properties": {
-                "name": "白沙灣遊艇會",
-                "id": 7,
-                "Latitude": 22.36479,
-                "Longitude": 114.259561
-            }
-        },
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    114.1783981,
-                    22.2447309,
-                    0
-                ]
-            },
-            "properties": {
-                "name": "香港鄉村俱樂部",
-                "id": 8,
-                "Latitude": 22.2447309,
-                "Longitude": 114.1783981
-            }
-        },
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    114.1933761,
-                    22.2585687,
-                    0
-                ]
-            },
-            "properties": {
-                "name": "香港木球會",
-                "id": 9,
-                "Latitude": 22.2585687,
-                "Longitude": 114.1933761
-            }
-        },
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    114.1821136,
-                    22.2753609,
-                    0
-                ]
-            },
-            "properties": {
-                "name": "香港足球會",
-                "id": 10,
-                "Latitude": 22.2753609,
-                "Longitude": 114.1821136
-            }
-        },
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    114.1864104,
-                    22.2464065,
-                    0
-                ]
-            },
-            "properties": {
-                "name": "香港哥爾夫球會深水灣高球場",
-                "id": 11,
-                "Latitude": 22.2464065,
-                "Longitude": 114.1864104
-            }
-        },
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    114.0902011,
-                    22.4419113,
-                    0
-                ]
-            },
-            "properties": {
-                "name": "香港哥爾夫球會粉嶺高球場",
-                "id": 12,
-                "Latitude": 22.4419113,
-                "Longitude": 114.0902011
-            }
-        },
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    114.1054379,
-                    22.4060616,
-                    0
-                ]
-            },
-            "properties": {
-                "name": "香港槍會",
-                "id": 13,
-                "Latitude": 22.4060616,
-                "Longitude": 114.1054379
-            }
-        },
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    114.0203572,
-                    22.420457,
-                    0
-                ]
-            },
-            "properties": {
-                "name": "香港機械模型會模型飛機場",
-                "id": 14,
-                "Latitude": 22.420457,
-                "Longitude": 114.0203572
-            }
-        },
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    114.1747692,
-                    22.3073598,
-                    0
-                ]
-            },
-            "properties": {
-                "name": "九龍印度會",
-                "id": 15,
-                "Latitude": 22.3073598,
-                "Longitude": 114.1747692
-            }
-        },
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    114.1887315,
-                    22.2753192,
-                    0
-                ]
-            },
-            "properties": {
-                "name": "印度遊樂會",
-                "id": 16,
-                "Latitude": 22.2753192,
-                "Longitude": 114.1887315
-            }
-        },
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    114.1916113,
-                    22.2694835,
-                    0
-                ]
-            },
-            "properties": {
-                "name": "香港渣甸山居民協會",
-                "id": 17,
-                "Latitude": 22.2694835,
-                "Longitude": 114.1916113
-            }
-        },
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    114.1740788,
-                    22.3036119,
-                    0
-                ]
-            },
-            "properties": {
-                "name": "九龍草地滾球會",
-                "id": 18,
-                "Latitude": 22.3036119,
-                "Longitude": 114.1740788
-            }
-        },
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    114.1737073,
-                    22.3049796,
-                    0
-                ]
-            },
-            "properties": {
-                "name": "九龍木球會",
-                "id": 19,
-                "Latitude": 22.3049796,
-                "Longitude": 114.1737073
-            }
-        },
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    114.1778408,
-                    22.3293961,
-                    0
-                ]
-            },
-            "properties": {
-                "name": "九龍塘會",
-                "id": 20,
-                "Latitude": 22.3293961,
-                "Longitude": 114.1778408
-            }
-        },
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    114.1798405,
-                    22.3322877,
-                    0
-                ]
-            },
-            "properties": {
-                "name": "九龍仔業主會",
-                "id": 21,
-                "Latitude": 22.3322877,
-                "Longitude": 114.1798405
-            }
-        },
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    114.1777844,
-                    22.3107764,
-                    0
-                ]
-            },
-            "properties": {
-                "name": "巴基斯坦協會",
-                "id": 22,
-                "Latitude": 22.3107764,
-                "Longitude": 114.1777844
-            }
-        },
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    114.1808949,
-                    22.2825149,
-                    0
-                ]
-            },
-            "properties": {
-                "name": "香港遊艇會(奇力島)",
-                "id": 23,
-                "Latitude": 22.2825149,
-                "Longitude": 114.1808949
-            }
-        },
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    114.189384,
-                    22.2378741,
-                    0
-                ]
-            },
-            "properties": {
-                "name": "香港遊艇會(熨波洲)",
-                "id": 24,
-                "Latitude": 22.2378741,
-                "Longitude": 114.189384
-            }
-        },
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    114.2684496,
-                    22.3694235,
-                    0
-                ]
-            },
-            "properties": {
-                "name": "香港遊艇會(西貢)",
-                "id": 25,
-                "Latitude": 22.3694235,
-                "Longitude": 114.2684496
-            }
-        },
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    114.2976533,
-                    22.3918219,
-                    0
-                ]
-            },
-            "properties": {
-                "name": "域多利遊樂會",
-                "id": 26,
-                "Latitude": 22.3918219,
-                "Longitude": 114.2976533
-            }
-        },
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    114.1730571,
-                    22.3315895,
-                    0
-                ]
-            },
-            "properties": {
-                "name": "又一村花園俱樂部",
-                "id": 27,
-                "Latitude": 22.3315895,
-                "Longitude": 114.1730571
-            }
+        "properties": {
+          "id": 27,
+          "zoomlvl": 16
         }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            114.301661,
+            22.267007, 
+            0
+          ]
+        },
+        "properties": {
+          "id": 16,
+          "zoomlvl": 15
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            114.1758149,
+            22.3059602,
+            0
+          ]
+        },
+        "properties": {
+          "id": 12,
+          "zoomlvl": 15
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            114.2597729,
+            22.3647875,
+            0
+          ]
+        },
+        "properties": {
+          "id": 6,
+          "zoomlvl": 16
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            114.178398,
+            22.244731,
+            0
+          ]
+        },
+        "properties": {
+          "id": 26,
+          "zoomlvl": 16
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            114.193376,
+            22.258569,
+            0
+          ]
+        },
+        "properties": {
+          "id": 23,
+          "zoomlvl": 16
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            114.1816527,
+            22.2751327,
+            0
+          ]
+        },
+        "properties": {
+          "id": 20,
+          "zoomlvl": 16
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            114.1869035,
+            22.2461577,
+            0
+          ]
+        },
+        "properties": {
+          "id": 24,
+          "zoomlvl": 17
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            114.1171,
+            22.491701,
+            0
+          ]
+        },
+        "properties": {
+          "id": 1,
+          "zoomlvl": 15
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            114.1016185,
+            22.3907167,
+            0
+          ]
+        },
+        "properties": {
+          "id": 3,
+          "zoomlvl": 16
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            114.0429787,
+            22.4160286,
+            0
+          ]
+        },
+        "properties": {
+          "id": 2,
+          "zoomlvl": 16
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            114.1900624,
+            22.2770167,
+            0
+          ]
+        },
+        "properties": {
+          "id": 23,
+          "zoomlvl": 17
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            114.1918417,
+            22.2688436,
+            0
+          ]
+        },
+        "properties": {
+          "id": 22,
+          "zoomlvl": 16
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            114.1768145,
+            22.3308788,
+            0
+          ]
+        },
+        "properties": {
+          "id": 9,
+          "zoomlvl": 15
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            114.1820966,
+            22.2842871,
+            0
+          ]
+        },
+        "properties": {
+          "id": 17,
+          "zoomlvl": 16
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            114.1874984,
+            22.2355924,
+            0
+          ]
+        },
+        "properties": {
+          "id": 25,
+          "zoomlvl": 16
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            114.2688765,
+            22.3683004,
+            0
+          ]
+        },
+        "properties": {
+          "id": 5,
+          "zoomlvl": 16
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            114.2971219,
+            22.3887617,
+            0
+          ]
+        },
+        "properties": {
+          "id": 4,
+          "zoomlvl": 16
+        }
+      }
     ]
-}
+  }
